ebf3dd7d47a993533518814c48da64e07289fc5c
Tom <tom.scheuer1993@gmail.com>
2024-09-28 02:50:52 +0200

    re

--- azureproject/production.py
+++ azureproject/production.py
@@ -5,10 +5,8 @@ from .settings import BASE_DIR
 
 # Configure the domain name using the environment variable
 # that Azure automatically creates for us.
-ALLOWED_HOSTS = os.environ('ALLOWED_HOSTS').split(' ')
-CSRF_TRUSTED_ORIGINS = os.environ('CSRF_TRUSTED_ORIGINS').split(' ')
-SECURE_SSL_REDIRECT=1
-
+ALLOWED_HOSTS = [os.environ['WEBSITE_HOSTNAME']] if 'WEBSITE_HOSTNAME' in os.environ else []
+CSRF_TRUSTED_ORIGINS = ['https://' + os.environ['WEBSITE_HOSTNAME']] if 'WEBSITE_HOSTNAME' in os.environ else []
 DEBUG = False
 
 # WhiteNoise configuration
