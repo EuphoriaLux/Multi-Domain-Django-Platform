17598d92504e8e838a39473a627846494ccdf4e3
Tom <tom.scheuer1993@gmail.com>
2025-02-23 22:18:17 +0100

    update

---
+++ azureproject/middleware.py
@@ -0,0 +1,22 @@
+# azureproject/middleware.py
+
+class DomainURLRoutingMiddleware:
+    """
+    Middleware that sets request.urlconf based on the HTTP host.
+    - For travelinstyle.lu (or www.travelinstyle.lu), use the travelinstyle URL configuration.
+    - For powerup.lu (or www.powerup.lu), use the powerup URL configuration.
+    - Otherwise, fallback to powerup (default).
+    """
+    def __init__(self, get_response):
+        self.get_response = get_response
+
+    def __call__(self, request):
+        host = request.get_host().split(':')[0].lower()  # Remove any port and lower-case the host
+        if host in ['travelinstyle.lu', 'www.travelinstyle.lu']:
+            request.urlconf = 'azureproject.urls_travelinstyle'
+        elif host in ['powerup.lu', 'www.powerup.lu']:
+            request.urlconf = 'azureproject.urls_powerup'
+        else:
+            # Fallback to powerup if no match
+            request.urlconf = 'azureproject.urls_powerup'
+        return self.get_response(request)
--- azureproject/urls_powerup.py
+++ azureproject/urls_powerup.py
@@ -1,10 +1,8 @@
-
-"""azureporject URL Configuration"""
+# azureproject/urls_powerup.py
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-from django.conf.urls.i18n import i18n_patterns
 
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
@@ -13,20 +11,24 @@ from rest_framework_simplejwt.views import (
 from entreprinder import views as entreprinder_views
 
 urlpatterns = [
+    # Include the i18n URLs so that {% url 'set_language' %} works
     path('i18n/', include('django.conf.urls.i18n')),
-    path('accounts/', include('allauth.urls')),  # Ensure this is outside i18n_patterns
-    path('login_complete/', entreprinder_views.login_complete, name='login_complete'),  # Add this line
+    
+    # You can also map set_language directly if you prefer:
+    # path('set_language/', include('django.conf.urls.i18n')),
 ]
 
-urlpatterns += i18n_patterns(
+urlpatterns += [
     path('admin/', admin.site.urls),
     path('', entreprinder_views.home, name='home'),
     path('', include('entreprinder.urls')),
+    path('accounts/', include('allauth.urls')),
+    path('login_complete/', entreprinder_views.login_complete, name='login_complete'),
     path('matching/', include('matching.urls')),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('api/protected/', entreprinder_views.protected_api, name='protected_api'),
-)
+]
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
