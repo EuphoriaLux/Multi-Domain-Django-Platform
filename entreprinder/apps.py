151597f97faea5dcfbd9cc4db40d7489ac940303
Tom <tom.scheuer1993@gmail.com>
2025-03-03 20:55:58 +0100

    update

--- entreprinder/apps.py
+++ entreprinder/apps.py
@@ -1,8 +1,10 @@
+# entreprinder/apps.py
 from django.apps import AppConfig
 
 class EntreprinderConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'entreprinder'
-
+    
     def ready(self):
-        import entreprinder.signals  # Import the signals
+        # Import and register without custom adapter for now
+        pass
